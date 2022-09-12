03c8672298f85409f926eda5f4ab1caf6271effa
Marcio Santetti <santetti@gmail.com>
2022-09-12 11:21:20 -0400

    monday commit

--- lectures/001-capitalism/001-aux.R
+++ lectures/001-capitalism/001-aux.R
@@ -4,27 +4,15 @@ library(tidyquant)
 library(hrbrthemes)
 library(scales)
 library(readxl)
-library(gapminder)
+library(ggeasy)
+library(viridis)
+library(MetBrewer)
 
 theme_set(theme_ipsum_rc())
 
 
 ####
 
-cpi <- read_csv("cpi.csv")
-
-
-
-cpi <- cpi %>% 
-  mutate(period = seq(ymd("1947-1-1"), ymd("2022-6-1"), by = "month"))
-
-cpi %>% 
-  ggplot(aes(x = period, y = cpi)) +
-  geom_line(size = 0.8)
-
-
-
-cpi %>% tail()
 
 
 
@@ -59,11 +47,22 @@ fred_raw %>%
 dat <- read_xlsx("maddison.xlsx", sheet = 3)
 
 dat_sample <- dat %>% 
-  filter(country %in% c("United Kingdom", "China", "Japan", "India", "Italy"))
+  filter(country %in% c("United Kingdom", "China", "Japan", "India", "Italy")) %>% 
+  filter(year >= 1000)
 
 dat_sample %>% 
   ggplot(aes(x = year, y = gdppc, color = country)) +
-  geom_line()
+  geom_line(size = 0.8) +
+  scale_y_continuous(labels=scales::dollar_format()) +
+  scale_color_met_d("Homer2") +
+  labs(x = "", y = "GDP per capita", title = "Gross domestic product per capita, selected countries (1000-2018)") +
+  easy_legend_at("bottom") +
+  easy_remove_legend_title() +
+  easy_plot_legend_size(13) +
+  easy_y_axis_title_size(13)
+
+
+###
 
 
 
@@ -71,6 +70,12 @@ co2 <- read_csv("co2.csv")
 
 
 co2 %>% 
-  filter(country %in% c("United Kingdom", "United States")) %>% 
+  filter(country %in% c("United Kingdom", "United States", "China", "Brazil")) %>% 
   ggplot(aes(x = year, y = co2_per_capita, color = country)) +
-  geom_line()
+  geom_line(size = .8, alpha = 0.6) +
+  easy_legend_at(to = "bottom") +
+  labs(x = "", y = "Emissions per capita", title = "Carbon emissions per capita, 1800-2020") +
+  easy_remove_legend_title() +
+  easy_y_axis_title_size(13) +
+  easy_plot_legend_size(13) +
+  scale_color_met_d("Degas")

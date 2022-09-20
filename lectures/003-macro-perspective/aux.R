e95e78d87fb478eca38db9a6a437d670a6af47a7
Marcio Santetti <santetti@gmail.com>
2022-09-20 08:46:36 -0400

    09/20

---
+++ lectures/003-macro-perspective/aux.R
@@ -0,0 +1,269 @@
+---
+  
+  # The size of the economy
+  
+  One caution when computing GDP is to avoid .hi[double counting].
+
+--
+  
+  Counting output more than once is a statistical .hi-orange[mistake], and may overstate the size of an economy.
+
+--
+  
+  <br>
+  
+  For instance, suppose a farmer sells oats to a miller by $50. The miller, then, refines it and produces oat flour. A bakery's owner purchases this oat flour by $100. This bakery sells oat cakes adding up to $200 per week. 
+
+--
+
+.right[
+Which of these values goes into GDP accounting?
+]
+
+--
+
+<br>
+
+.hi[Intermediate, illegal], and .hi[used] goods, as well as .hi[transfer payments] are .hi-orange[not] accounted for in GDP.
+
+---
+
+layout: false
+class: inverse, middle
+
+# Real vs. nominal GDP
+
+
+---
+
+# Real vs. nominal GDP
+
+A .hi[crucial] distinction in Economics is between .hi-orange[real] and .hi-orange[nominal] values.
+
+--
+
+For the case of GDP, this is especially important when .hi[inflation] is such an important component of the economy.
+
+--
+
+> .hi-orange[Inflation] is a *sustained* increase in sn economy's price level.
+
+--
+  
+  <br>
+  
+  The .hi[nominal] value of any economic measure means implies a statistic in terms of .hi-orange[actual prices] that exist at the time.
+
+--
+  
+  On the other hand, .hi[real] values refer to the *same* statistic after it has been .hi-orange[adjusted for inflation].
+
+
+---
+  
+  # Real vs. nominal GDP
+  
+  Simply looking at *nominal* values when evaluating economic statistics may be misleading.
+
+--
+  
+  Consider the number of cars sold, for instance.
+
+--
+  
+  * Does an SUV have the *same* prce today, relative to 1985?
+  
+  --
+  
+  <br>
+  
+  
+  
+  Thus, to have a more *accurate* basis for evaluating economic growth, economists prefer .hi[real] measures, as they account for inflation.
+
+--
+  
+  For the case of GDP, we use the .hi-orange[GDP deflator] to .hi[*normalize*] nominal GDP values.
+
+--
+  
+  
+  > The .hi[GDP deflator] is a price index measuring the average prices of all final goods and services included in the economy.
+
+---
+  
+  # Real vs. nominal GDP
+  
+  [put table 6.5]
+
+
+calculate real gdp from it.
+
+$$
+  \begin{aligned}
+\text{Real GDP} = \dfrac{\text{Nominal GDP}}{\text{GDP Deflator}}
+\end{aligned}
+$$
+  
+  ---
+  
+  # Real vs. nominal GDP
+  
+  [And a plot comparing the two]
+
+
+---
+  layout: false
+class: inverse, middle
+
+# GDP over time
+
+
+---
+  
+  # GDP over time
+  
+  
+  [first grapha again]
+
+
+---
+  
+  # GDP over time
+  
+  A .hi[recession] happens whenever significant short-term declines interrupt a general upward long-term trend of GDP.
+
+--
+  
+  In cases of *lenghty* and *deep* recessions, the economy experiences a .hi-orange[depression].
+
+
+<br>
+  
+  .right[
+    Do you recall any *examples* of depressions and/or recessions?
+  ]
+
+
+--
+  
+  An economy's movement from peak to trough and trough to peak the .hi[business cycle].
+
+---
+layout: false
+class: inverse, middle
+
+# Same measure, different countries
+
+
+---
+
+# Same measure, different countries
+
+How do we know if an economy is doing well?
+
+--
+
+One way to go about it is to compare itsv economic perfomance with previous years.
+
+--
+
+However, comparing economic performance .hi[among different countries] is a powerful tool for economists, especially in periods of expansions and recessions.
+
+--
+
+Recall the concept of .hi-orange[Nominal] GDP.
+
+  * Given that we do not have a uniform currency around the globe, how do we compare the .hi[*same*] statistic (GDP) for countries with .hi[*different*] currencies (e.g., dollar, euro, yen, peso, etc.)?
+  
+--
+
+Using an .hi[exchange rate]&mdash;the value of one currency in terms of *another* currency, is a way to compare the same economic measure across different countries.
+
+
+---
+
+# Same measure, different countries
+
+One of the main common denominators for comparing different countries are .hi[purchasing power parity] (PPP) equivalent exchange rates. 
+
+--
+
+Suppose country *A*'s GDP in 2020 was $2.0 trillion, while country *B*'s was $5 trilion.
+
+--
+
+Country *A*'s currency is called *z*, while country *B*'s currency is *y*.
+
+--
+
+How much is country *A*'s GDP *in terms of* country *B*'s currency?
+
+--
+
+Given that 1.70 *z* = 1 *y*, we have
+
+
+
+$$
+\begin{aligned}
+\text{A's GDP in y units} = \dfrac{\text{A's Nominal GDP}}{\text{Exchange rate (z/y)}}
+\end{aligned}
+$$
+
+--
+
+$$
+\begin{aligned}
+\text{A's GDP in y units} = \dfrac{2.0}{1.70} = 1.176 \ \text{trillion}
+\end{aligned}
+$$
+  
+  ---
+  
+  # Same measure, different countries
+  
+  What about countries with different .hi[population sizes]?
+  
+  
+  --
+  
+  The simplest way to evaluate a country's GDP taking into account its population size is to compute teh GDP *per capita*.
+
+<br>
+
+$$
+\begin{aligned}
+\text{GDP per capita} = \dfrac{\text{GDP}}{\text{Population size}}
+\end{aligned}
+$$
+
+
+---
+layout: false
+class: inverse, middle
+
+# GDP vs. well-being
+
+---
+
+# GDP vs. well-being
+
+<br>
+
+Do you believe that a country's GDP *accurately* measures how well its population is?
+  
+--
+  
+  <br>
+  
+  
+  [now more conditions to evaluate whether gdp implies higher living standards -- had this discussion in the first week.]
+
+
+---
+  
+  # GDP vs. well-being
+  
+  
+  Moral of the story: no measure is perfect. we have to be critical about it and evaluate them alongside other measures, according to our interests.
